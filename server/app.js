5583e6b4afa6175a37820183c8efecfbb4b7c860
Jintian Pan <jintianpan@nat-10-20-150-253.uofm.wireless.umn.edu>
2017-01-03 16:14:54 -0600

    m

--- server/app.js
+++ server/app.js
@@ -25,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', routes);
-app.use(passport.initialize());
 app.use('/api', server);
 
 var reactBase = path.resolve(__dirname, '../client/build')

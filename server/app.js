823c5a91c2860413582e058cdb9eb167d5cb77e3
Jintian Pan <jintianpan@nat-10-20-150-253.uofm.wireless.umn.edu>
2017-01-03 15:59:17 -0600

    m

--- server/app.js
+++ server/app.js
@@ -49,8 +49,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.send({
+    res.status(err.status || 500).send({
       message: err.message,
       error: err
     });
@@ -60,7 +59,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.send('error', {
+  res.status(err.status || 500).send({
     message: err.message,
     error: {}
   });

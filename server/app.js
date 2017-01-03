020c9b88756c1b56b10012d0d5290fae65e4ae1b
Jintian Pan <jintianpan@nat-10-20-150-253.uofm.wireless.umn.edu>
2017-01-03 16:23:34 -0600

    m

--- server/app.js
+++ server/app.js
@@ -5,10 +5,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-const passport = require('passport');
 
 var routes = require('./src/index');
 var server = require('./src/server');
+
 
 var app = express();
 
@@ -34,6 +34,10 @@ if (!fs.existsSync(reactBase)) {
 app.use('/static', express.static(path.join(reactBase, 'static')));
 // app.use(express.static(reactBase));
 var indexFile = path.join(reactBase, 'index.html')
+app.use(function(req, res, next) {
+  // TODO - catch errors http://expressjs.com/en/api.html#res.sendFile
+  res.sendFile(indexFile);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,11 +47,13 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
+
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500).send({
+    res.status(err.status || 500);
+    res.send({
       message: err.message,
       error: err
     });
@@ -57,7 +63,8 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500).send({
+  res.status(err.status || 500);
+  res.send('error', {
     message: err.message,
     error: {}
   });
